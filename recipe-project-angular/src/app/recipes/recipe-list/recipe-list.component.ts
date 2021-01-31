88c2c2cedbd9dc7429bfe5b6476805ec18eea1ab
smreeti <mool.smreeti@gmail.com>
2021-01-31 09:15:36 +0545

    add recipe list

--- recipe-project-angular/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-project-angular/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe('Spagetthi', 'This is Spagetthi',
+      'https://www.thewholesomedish.com/wp-content/uploads/2020/08/THE-BEST-CLASSIC-SPAGHETTI-600X900.jpg'),
+    new Recipe('Spagetthi', 'This is Spagetthi',
+      'https://www.thewholesomedish.com/wp-content/uploads/2020/08/THE-BEST-CLASSIC-SPAGHETTI-600X900.jpg')
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
