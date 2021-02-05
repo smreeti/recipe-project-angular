182714902dd513b19850292de7fd92b10385772d
smreeti <mool.smreeti@gmail.com>
2021-02-05 16:31:21 +0545

    adding user to add ingredients

--- recipe-project-angular/src/app/app.component.ts
+++ recipe-project-angular/src/app/app.component.ts
@@ -1,13 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'recipe-project-angular';
   loadedFeature = 'recipe';
+
+  ngOnInit(): void {
+    console.log('called:::::::');
+  }
 
   onNavigate = (feature: string) => {
     this.loadedFeature = feature;
