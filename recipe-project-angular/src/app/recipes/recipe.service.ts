d0cc2b6fb3a7f095c87eb604311582dced5a9239
smreeti <mool.smreeti@gmail.com>
2021-02-07 08:59:50 +0545

    adding ingredientrs to recipe

--- recipe-project-angular/src/app/recipes/recipe.service.ts
+++ recipe-project-angular/src/app/recipes/recipe.service.ts
@@ -1,13 +1,24 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe('Spagetthi', 'This is Spagetthi',
-      'https://www.thewholesomedish.com/wp-content/uploads/2020/08/THE-BEST-CLASSIC-SPAGHETTI-600X900.jpg'),
-    new Recipe('Momos', 'This is Momo',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpCQixzITHVHtn8kYQQ2oRsiexqn-Fu7dIeQ&usqp=CAU')
+    new Recipe('Spagetthi',
+      'This is Spagetthi',
+      'https://www.thewholesomedish.com/wp-content/uploads/2020/08/THE-BEST-CLASSIC-SPAGHETTI-600X900.jpg',
+      [
+        new Ingredient('Noodles', 1),
+        new Ingredient('Pasta Sauce', 1)
+      ]),
+    new Recipe('Momos',
+      'This is Momo',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpCQixzITHVHtn8kYQQ2oRsiexqn-Fu7dIeQ&usqp=CAU',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Flour', 1)
+      ])
   ];
   recipeSelected = new EventEmitter<Recipe>();
   getRecipes = () => {
