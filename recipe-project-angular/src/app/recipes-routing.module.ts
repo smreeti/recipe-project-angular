2709efc99b9a6f555d11f213125663da8b387ad0
smreeti <mool.smreeti@gmail.com>
2021-02-14 13:53:38 +0545

    outsource recipe module

---
+++ recipe-project-angular/src/app/recipes-routing.module.ts
@@ -0,0 +1,42 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RecipesComponent} from '../recipes/recipes.component';
+import {RecipeStartComponent} from '../recipes/recipe-start/recipe-start.component';
+import {RecipeEditComponent} from '../recipes/recipe-edit/recipe-edit.component';
+import {RecipeDetailComponent} from '../recipes/recipe-detail/recipe-detail.component';
+
+const recipeRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
+      {
+        path: '',
+        component: RecipeStartComponent
+      },
+      {
+        path: 'new',
+        component: RecipeEditComponent
+      },
+      {
+        path: ':id',
+        component: RecipeDetailComponent
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent
+      },
+    ]
+  }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(recipeRoutes)],
+  exports: [RouterModule]
+})
+export class RecipesRoutingModule {
+}
